6540fc64e71985bbb575ab50bcb7ac2cee6651c6
Kelley Chaplain <kmchaplain@gmail.com>
2019-06-03 23:32:46 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,33 @@
+require 'pry'
+
+class Artist
+  attr_accessor :name, :songs
+  @@all = []
+  
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def add_song(song)
+    @songs << song
+  end 
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.find_or_create_by_name(name)
+    self.name ||= self.name 
+  end
+  
+  def print_songs
+    @songs
+  end  
+
+end  
---
+++ lib/mp3_importer.rb
@@ -0,0 +1,11 @@
+class MP3Importer
+  attr_accessor :path
+  def initialize(file_path)
+    @path = file_path
+  end
+  
+  def files
+    
+  end  
+
+end  
